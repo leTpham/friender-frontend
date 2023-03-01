53fb27efcbbd773698dfdcb48c4c749475cddecf
Le Pham <phamtle724@gmail.com>
2023-03-01 09:41:55 -0800

    restyle login form

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
+import {Form, FormGroup, Input, Label, Button} from "reactstrap"
 import userContext from "./userContext";
+import "./LoginForm.css"
 
 /** Form for logging in.
  *
@@ -41,48 +42,133 @@ function LoginForm({ login }) {
   }
 
   return (
-    <div className="loginPage container mt-5">
-      <h3 className="mb-4">Log In</h3>
-      <form className="LoginForm" onSubmit={handleSubmit}>
+    // <div className="loginPage container mt-5">
+    //   <h3 className="mb-4">Log In</h3>
+    //   <form className="LoginForm" onSubmit={handleSubmit}>
 
-        <div className="mb-3">
-          <label className="mb-2 label">Username</label>
-          <input
-            id="username"
-            name="username"
-            className="form-control"
-            placeholder="Enter username"
-            onChange={handleChange}
-            value={formData.username}
-            aria-label="Username"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="mb-2 label">Password</label>
-          <input
-            id="password"
-            name="password"
-            className="form-control"
-            placeholder="Enter password"
-            onChange={handleChange}
-            value={formData.password}
-            aria-label="Password"
-            type="password"
-          />
-        </div>
-        {!isBadLogin &&
+    //     <div className="mb-3">
+    //       <label className="mb-2 label">Username</label>
+    //       <input
+    //         id="username"
+    //         name="username"
+    //         className="form-control"
+    //         placeholder="Enter username"
+    //         onChange={handleChange}
+    //         value={formData.username}
+    //         aria-label="Username"
+    //       />
+    //     </div>
+    //     <div className="mb-3">
+    //       <label className="mb-2 label">Password</label>
+    //       <input
+    //         id="password"
+    //         name="password"
+    //         className="form-control"
+    //         placeholder="Enter password"
+    //         onChange={handleChange}
+    //         value={formData.password}
+    //         aria-label="Password"
+    //         type="password"
+    //       />
+    //     </div>
+    //     {!isBadLogin &&
+    //       <div className="alert alert-danger" role="alert">
+    //         Incorrect Username or Password
+    //       </div>
+    //     }
+    //     <div className="mb-3">
+    //       <button className="btn btn-primary" onClick={handleSubmit}>
+    //         Submit
+    //       </button>
+    //     </div>
+
+    //   </form>
+    // </div>
+    <div className="loginPage">
+    <Form className="loginForm">
+      <FormGroup floating>
+        <Input
+        id="username"
+        name="username"
+        className="form-control"
+        placeholder="Enter username"
+        onChange={handleChange}
+        value={formData.username}
+        aria-label="Username"
+      />
+      <Label for="username">
+        Username
+      </Label>
+      </FormGroup>
+      {' '}
+      <FormGroup floating>
+        <Input
+        id="password"
+        name="password"
+        className="form-control"
+        placeholder="Enter password"
+        onChange={handleChange}
+        value={formData.password}
+        aria-label="Password"
+        type="password"
+      />
+      <Label for="password">
+        Password
+      </Label>
+      </FormGroup>
+    {' '}
+    {!isBadLogin &&
           <div className="alert alert-danger" role="alert">
             Incorrect Username or Password
           </div>
         }
-        <div className="mb-3">
-          <button className="btn btn-primary" onClick={handleSubmit}>
-            Submit
-          </button>
-        </div>
-
-      </form>
+    <Button onClick={handleSubmit}>
+      Submit
+    </Button>
+    </Form>
     </div>
+    // <div className="loginPage container mt-5">
+    //   <h3 className="mb-4">Log In</h3>
+    //   <form className="LoginForm" onSubmit={handleSubmit}>
+
+    //     <div className="mb-3">
+    //       <label className="mb-2 label">Username</label>
+    //       <input
+    //         id="username"
+    //         name="username"
+    //         className="form-control"
+    //         placeholder="Enter username"
+    //         onChange={handleChange}
+    //         value={formData.username}
+    //         aria-label="Username"
+    //       />
+    //     </div>
+    //     <div className="mb-3">
+    //       <label className="mb-2 label">Password</label>
+    //       <input
+    //         id="password"
+    //         name="password"
+    //         className="form-control"
+    //         placeholder="Enter password"
+    //         onChange={handleChange}
+    //         value={formData.password}
+    //         aria-label="Password"
+    //         type="password"
+    //       />
+    //     </div>
+    //     {!isBadLogin &&
+    //       <div className="alert alert-danger" role="alert">
+    //         Incorrect Username or Password
+    //       </div>
+    //     }
+    //     <div className="mb-3">
+    //       <button className="btn btn-primary" onClick={handleSubmit}>
+    //         Submit
+    //       </button>
+    //     </div>
+
+    //   </form>
+    // </div>
   );
 }
 
